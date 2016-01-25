df2bee4f03ae87a57ef925365395d645d174d3dd
José Manuel Snyder <code@3nyder.com>
2016-01-24 18:55:13 -0600

    split data

--- boston_housing.py
+++ boston_housing.py
@@ -9,6 +9,7 @@ from sklearn.tree import DecisionTreeRegressor
 ################################
 ### ADD EXTRA LIBRARIES HERE ###
 ################################
+from sklearn import cross_validation
 
 
 def load_data():
@@ -48,9 +49,8 @@ def split_data(city_data):
     # Get the features and labels from the Boston housing data
     X, y = city_data.data, city_data.target
 
-    ###################################
-    ### Step 2. YOUR CODE GOES HERE ###
-    ###################################
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+        X, y, test_size=0.30, random_state=333)
 
     return X_train, y_train, X_test, y_test
 
